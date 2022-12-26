ce896c27a0dbef6d4df4f588abc6d92b68579d82
Rashid Mohamad <>
2022-12-26 13:11:13 +0530

    Initial code ...

--- src/collection.ts
+++ src/collection.ts
@@ -1,35 +1,37 @@
-export class Collection<T> implements Iterable<T>, Iterator<T> {
+type CollectionItemType<T> = any | T[];
+
+export class Collection<T> implements Iterable<any>, Iterator<any> {
 
     private currentIndex: number = 0;
-    private items: Map<string, T> = new Map();
+    protected items: CollectionItemType<T>;
     private isArray = true;
 
     constructor(items: T[] | any) {
-        if (Array.isArray(items)) {
-            for (let index = 0; index < items.length; index++) {
-                this.items.set(index + '', items[index]);
-            }
-        } else {
-            this.isArray = false;
-            const keys = Object.keys(items);
-            for (let key of keys) {
-                this.items.set(key, items[key]);
-            }
-        }
+        this.isArray = Array.isArray(items);
+        this.items = items;
     }
 
-    get length() {
-        return this.items.size;
+    private get itemsAsArray():T[] {
+        return this.items as T[];
+    }
+
+    private get itemsAsObject():any {
+        return this.items as any;
+    }
+
+    get length():number {
+        return this.isArray ? this.itemsAsArray.length : Object.keys(this.itemsAsObject).length;
     }
 
     [Symbol.iterator](): Iterator<T, any, undefined> {
+        this.currentIndex = 0;
         return this;
     }
 
     next(...args: [] | [undefined]): IteratorResult<T, any> {
         if (this.currentIndex < this.length) {
             return {
-                value: this.items.get(this.keys().get('' + this.currentIndex++)!)!,
+                value: this.isArray ? this.itemsAsArray[this.currentIndex++] : this.itemsAsObject.get(this.keys().get('' + this.currentIndex++)!)!,
                 done: this.currentIndex == this.length,
             };
         }
@@ -43,8 +45,8 @@ export class Collection<T> implements Iterable<T>, Iterator<T> {
      * Returns the underlying array represented by the collection.
      * @returns T[]
      */
-    all(): T[] | any {
-        return this.isArray ? [...this.items.values()] : Object.fromEntries(this.items);
+    all(): T[] | T {
+        return this.isArray ? this.itemsAsArray : this.itemsAsObject;
     }
 
     /**
@@ -65,44 +67,109 @@ export class Collection<T> implements Iterable<T>, Iterator<T> {
         return this.sum(key) / this.length;
     }
 
-    chunk(size: number): Collection<Collection<T>> {
-        return Collection.collect([...this.items.values()].reduce((acc: T[][], val: T, index: number) => {
-            const subIndex = Math.floor(index / size);
-            if (acc.length == subIndex) {
-                acc.push([val]);
-            } else {
-                acc[subIndex].push(val);
-            };
-            return acc;
-        }, []));
+    chunk(size: number): Collection<T[]> {
+        if(this.isArray) {
+
+
+            return collect(this.itemsAsArray.reduce((acc:T[][], val:T, ind:number) => {
+                const subIndex = Math.floor(ind / size);
+                if(acc.length == subIndex) {
+                    acc.push([val]);
+                 } else {
+                    acc[subIndex].push(val);
+                 }
+                 return acc;
+            }, []));
+        } else {
+            const keys = this.keys().all() as string[];
+            return collect(keys.reduce((acc:any[], key:string, ind:number) => {
+                const subIndex = Math.floor(ind / size);
+                if(acc.length == subIndex) {
+                    acc.push({
+                        [key]: this.itemsAsObject[key],
+                    });
+                } else {
+                    acc[subIndex][key] = this.itemsAsObject[key];
+                }
+                return acc;
+            }, [] as any[]));
+        }
     }
 
-    /* chunkWhile(callback: ((value: T, index: string, chunk: T[]) => boolean)) {
-        const chunks: T[][] = [Collection.collect<T>([])];
-        for (let index = 0; index < this.items.length; index++) {
-            if (callback())
-        }
-    } */
+    chunkWhile(callback: ((value: any, index: string, chunk: Collection<T>) => boolean)) {
+        const chunks: any[][] = [];
+        this.each((v, k) => {
+            if (callback(v, k, collect(chunks.length > 0 ? chunks[chunks.length - 1] : []))) {
+                chunks[chunks.length - 1].push(v);
+            } else {
+                chunks.push([v]);
+            }
+        });
+        return collect(chunks);
+    }
 
-    each(callback: ((value: T, key: string) => boolean | void)) {
-        for (let item of this.items) {
-            const result = callback(item[1], item[0]);
-            if (result === false) {
-                break;
+    collapse<V>() {
+        return collect((this.all() as any[]).flat() as V[]);
+    }
+
+    collect() {
+        return collect(this.isArray ? this.itemsAsArray : this.itemsAsObject);
+    }
+
+    combine<V>(items: V[]) {
+        const newObject: any = {};
+        this.each((v, k) => {
+            newObject[v] = typeof items[parseInt(k)] == 'undefined' ? null : items[parseInt(k)];
+        });
+        return collect(newObject);
+    }
+
+    contains(valueOrCallback: T | ((v: any, k: string) => boolean)) {
+        if (typeof valueOrCallback == 'string') {
+            return this.isArray ? this.itemsAsArray.includes(valueOrCallback) : Object.values(this.itemsAsObject).includes(valueOrCallback);
+        } else {
+            let result = false;
+            this.each((v:any, k) => {
+                if ((valueOrCallback as ((v: any, k: string) => boolean))(v, k)) {
+                    result = true;
+                    return false;
+                }
+            });
+            return result;
+        }
+    }
+
+    each(callback: ((value: any, key: string) => boolean | void)) {
+        if(this.isArray) {
+            for(let i = 0; i < this.length; i++) {
+                const result = callback(this.itemsAsArray[i], i + '');
+                if (result === false) {
+                    break;
+                }
+            }
+        } else {
+            for (let key of this.itemsAsObject) {
+                const result = callback(this.itemsAsObject[key], key);
+                if (result === false) {
+                    break;
+                }
             }
         }
     }
 
-    first(callback?: ((value: T, index: number) => boolean)): T | null {
+    first(callback?: ((value: any, index: number) => boolean)): any | null {
         if (callback === undefined) {
-            const values = [...this.items.values()];
-            return this.length > 0 ? values[0] : null;
+            if(this.isArray) {
+                return this.length > 0 ? this.itemsAsArray[0] : null;
+            } else {
+                return this.length > 0 ? this.itemsAsObject[Object.keys(this.itemsAsObject)[0]] : null;
+            }
         } else {
             const keys = this.keys();
             for (let keyIndex = 0; keyIndex < this.length; keyIndex++) {
-                const key = keys.get<string>(keyIndex + '')!;
-                const value = this.items.get(key)!;
-                if (callback(value, keyIndex)) {
+                const key = this.isArray ? (keyIndex + '') : keys.get<string>(keyIndex + '');
+                const value = this.get(key);
+                if (callback(value, key)) {
                     return value;
                 }
             }
@@ -110,27 +177,38 @@ export class Collection<T> implements Iterable<T>, Iterator<T> {
         return null;
     }
 
-    get<V>(key: string, defaultValue?: V): V | undefined {
-        if (this.items.has(key)) {
-            return this.items.get(key) as V;
+    get<V>(key: string, defaultValue?: V) {
+        if(this.isArray) {
+            const intKey = parseInt(key);
+            if(intKey < this.length) {
+                return this.itemsAsArray[intKey];
+            }
+        } else {
+            if(typeof this.itemsAsObject[key] != 'undefined') {
+                return this.itemsAsObject[key];
+            }
         }
         return defaultValue;
     }
 
     keys(): Collection<string> {
-        return Collection.collect([...this.items.keys()]);
+        return collect(Object.keys(this.items));
     }
 
-    last(callback?: ((value: T, index: number) => boolean)): T | null {
+    last(callback?: ((value: any, key: string) => boolean)): any | null {
         if (callback === undefined) {
-            const values = [...this.items.values()];
-            return this.length > 0 ? values[values.length - 1] : null;
+            if(this.isArray) {
+                return this.length > 0 ? this.itemsAsArray[this.length - 1] : null;
+            } else {
+                const keys = Object.keys(this.itemsAsObject);
+                return this.length > 0 ? this.itemsAsObject[keys[keys.length - 1]] : null;
+            }
         } else {
             const keys = this.keys();
             for (let keyIndex = this.length - 1; keyIndex > -1; keyIndex--) {
-                const key = keys.get<string>(keyIndex + '')!;
-                const value = this.items.get(key)!;
-                if (callback(value, keyIndex)) {
+                const key = this.isArray ? (keyIndex + '') : keys.get<string>(keyIndex + '');
+                const value = this.get(key);
+                if (callback(value, key)) {
                     return value;
                 }
             }
@@ -138,26 +216,57 @@ export class Collection<T> implements Iterable<T>, Iterator<T> {
         return null;
     }
 
-    map<V>(callback: ((value: T, key: string) => V)) {
-        if(this.isArray) {
-            return Collection.collect<V>(this.values().all().map(callback));
+    map<V>(callback: ((value: T, key: string) => V)):Collection<V> {
+        if (this.isArray) {
+            return collect(this.itemsAsArray.map((v, k) => callback(v, k + '')));
         } else {
-            const newObj:any = {};
+            const newObj: any = {};
             this.each((v, k) => {
                 newObj[k] = callback(v, k);
             });
-            return Collection.collect<V>(newObj);
+            return collect(newObj);
         }
     }
 
-    reduce<V>(callback: ((previousValue: V, currentValue: T) => V), initialValue: V) {
-        this.values().each((v, k) => {
+    push(...values: T[]) {
+        if (this.isArray) {
+            this.itemsAsArray.push(...values);
+        } else {
+            Collection.iterate(values, (v, k) => {
+                this.items[k] = v;
+            });
+        }
+        return this;
+    }
+
+    put(key: string, value: any) {
+        if(this.isArray) {
+            const intKey = parseInt(key);
+            if(intKey < this.length) {
+                this.itemsAsArray[intKey] = value;
+            } else {
+                this.itemsAsArray.push(value);
+            }
+        } else {
+            this.itemsAsObject[key] = value;
+        }
+        return this;
+    }
+
+    random() {
+        const keys = this.keys().all();
+        const randomKey = keys[Math.floor(Math.random() * keys.length)];
+        return this.items.get(randomKey);
+    }
+
+    reduce<V>(callback: ((previousValue: V, currentValue: any) => V), initialValue: V) {
+        this.values().each((v:T, k:string) => {
             initialValue = callback(initialValue, v);
         });
         return initialValue;
     }
 
-    sum(key?: string | ((item: T) => number)): number {
+    sum(key?: string | ((item: any) => number)): number {
         if (key === undefined || typeof key === 'string') {
             return this.reduce((pv: number, cv: any) => pv + (key === undefined ? (cv as number) : (cv[key] as number)), 0) as number;
         } else {
@@ -166,10 +275,25 @@ export class Collection<T> implements Iterable<T>, Iterator<T> {
     }
 
     values(): Collection<T> {
-        return Collection.collect([...this.items.values()]);
+        return collect(this.isArray ? this.itemsAsArray : Object.values(this.itemsAsObject));
     }
 
-    static collect<R>(items: R[] | any): Collection<R> {
-        return new Collection(items);
+    private static iterate<T>(arr: T[] | any, callback: ((v: T, k: string) => void)) {
+        const indexes = Object.keys(arr);
+        for (let index of indexes) {
+            callback(arr[index], index);
+        }
     }
+}
+
+export const collect = <T>(items: T[] | T):Collection<T> => new Collection(items);
+
+export const range = (start: number, end: number) => {
+    const from = parseInt(Math.min(start, end) + '');
+    const to = parseInt(Math.max(start, end) + '');
+    const items = [];
+    for (let i = from; i <= to; i++) {
+        items.push(i);
+    }
+    return collect(items);
 }
--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -1,68 +1,113 @@
-import { Collection } from "../src/collection";
+import { collect, range } from "../src/collection";
 
 describe('test collection iterator', () => {
 
-    test('iterator is generated', () => {
-        expect(Collection.collect([1, 2, 3]).next().value).toBe(1);
+    test('initialize with array', () => {
+        expect(collect([1, 2, 3]).all()).toEqual([1, 2, 3]);
+    });
+
+    test('initialize with object', () => {
+        expect(collect({
+            name: 'John',
+            age: 32,
+        }).all()).toEqual({
+            name: 'John',
+            age: 32,
+        });
     });
 
     test('can be iterated', () => {
-        for (let one of Collection.collect([1])) {
+        for (let one of collect([1])) {
             expect(one).toBe(1);
         }
     });
 });
 
 describe('sum functions', () => {
-    test('with numbers', () => {
-        expect(Collection.collect([1, 2, 3]).sum()).toBe(6);
+    test('with numbers array', () => {
+        expect(collect([1, 2, 3]).sum()).toBe(6);
     });
 
     test('with object key', () => {
-        expect(Collection.collect([
+        expect(collect([
             { 'name': 'JavaScript: The Good Parts', 'pages': 176 },
             { 'name': 'JavaScript: The Definitive Guide', 'pages': 1096 }
         ]).sum('pages')).toBe(1272);
     });
 
     test('with callback', () => {
-        expect(Collection.collect([
+        expect(collect([
             { 'name': 'Chair', 'colors': ['Black'] },
             { 'name': 'Desk', 'colors': ['Black', 'Mahogany'] },
             { 'name': 'Bookcase', 'colors': ['Red', 'Beige', 'Brown'] },
-        ]).sum((v:any) => v.colors.length)).toBe(6);
+        ]).sum((v: any) => v.colors.length)).toBe(6);
     });
 });
 
 describe('test miscelenous functions', () => {
     test('chunk function', () => {
-        expect(Collection.collect([1, 2, 3, 4, 5, 6, 7]).chunk(4).all()).toEqual([[1, 2, 3, 4], [5, 6, 7]]);
+        expect(collect([1, 2, 3, 4, 5, 6, 7]).chunk(4).all()).toEqual([[1, 2, 3, 4], [5, 6, 7]]);
+    });
+
+    test('chunkWhile function', () => {
+        expect(collect('AABBCCCD'.split('')).chunkWhile((v, k, chunk) => v === chunk.last()).all()).toEqual([['A', 'A'], ['B', 'B'], ['C', 'C', 'C'], ['D']]);
+    });
+
+    test('collapse function', () => {
+        expect(collect([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ]).collapse().all()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    test('combine function', () => {
+        expect(collect(['name', 'age']).combine(['George', 29]).all()).toEqual({
+            name: 'George',
+            age: 29
+        });
     });
 
     test('first element', () => {
-        expect(Collection.collect([1, 2, 3, 4, 5, 6, 7]).first()).toBe(1);
+        expect(collect([1, 2, 3, 4, 5, 6, 7]).first()).toBe(1);
     });
 
     test('first element with callback', () => {
-        expect(Collection.collect<number>([1, 2, 3, 4]).first((v:number, i) => v > 2)).toBe(3);
+        expect(collect([1, 2, 3, 4]).first((v, i) => (v as number) > 2)).toBe(3);
     });
 
     test('keys function', () => {
-        console.log(Collection.collect({
-            'prod-100': {'product_id': 'prod-100', 'name': 'Desk'},
-            'prod-200': {'product_id': 'prod-200', 'name': 'Chair'},
-        }).keys().all());
-        expect(Collection.collect({
-            'prod-100': {'product_id': 'prod-100', 'name': 'Desk'},
-            'prod-200': {'product_id': 'prod-200', 'name': 'Chair'},
+        expect(collect({
+            'prod-100': { 'product_id': 'prod-100', 'name': 'Desk' },
+            'prod-200': { 'product_id': 'prod-200', 'name': 'Chair' },
         }).keys().all()).toEqual(['prod-100', 'prod-200']);
     });
 
     test('last element', () => {
-        expect(Collection.collect<number>([1, 2, 3, 4, 5, 6, 7]).last()).toBe(7);
+        expect(collect([1, 2, 3, 4, 5, 6, 7]).last()).toBe(7);
     });
 
     test('last element with callback', () => {
-        expect(Collection.collect<number>([1, 2, 3, 4]).last((v, i) => v < 3)).toBe(2);
+        expect(collect([1, 2, 3, 4]).last((v, i) => v < 3)).toBe(2);
     });
+
+    test('push function', () => {
+        expect(collect([1, 2, 3, 4]).push(5).all()).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    test('put function', () => {
+        expect(collect({
+            product_id: 1,
+            name: 'Desk',
+        }).put('price', 100).all()).toEqual({
+            product_id: 1,
+            name: 'Desk',
+            price: 100
+        });
+    });
+
+    /* test('random function', () => {
+        const collection = range(1, 10);
+
+    }); */
 });
